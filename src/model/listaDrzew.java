a5dae02c8b8ea642af163d0dca58b465b9689648
andrzej <andrzej@andrzej-Lenovo-Y50-70>
2015-04-27 20:12:31 +0200

    HAmbagaa

--- src/model/listaDrzew.java
+++ src/model/listaDrzew.java
@@ -10,18 +10,19 @@ public class listaDrzew {
 	
 	private String predykaty;// bedzie zawieral predykaty
 	
+	private ArrayList<ArrayList<String>> tablicaSlow = new ArrayList<ArrayList<String>>();
+	
 	public listaDrzew()
 	{
 		
 	}
 	
-	
-	public void wypelnijListe()
+	public void dzielSlowa()
 	{
 		
-		ArrayList<ArrayList<String>> tablicaSlow = new ArrayList<ArrayList<String>>();
-		
-		
+		//ArrayList<ArrayList<String>> tablicaSlow = new ArrayList<ArrayList<String>>();
+	
+	
 		String przerywnik = "[\\n]+";
 		ArrayList<String> linie = new ArrayList<String>(Arrays.asList(predykaty.split(przerywnik)));
 		
@@ -31,77 +32,100 @@ public class listaDrzew {
 		{
 			tablicaSlow.add(new ArrayList<String>(Arrays.asList(linie.get(x).split(przerywnik))));
 		}
-               
-        System.out.println(tablicaSlow);
+	           
+	    System.out.println(tablicaSlow);
+	    
+	    
+	    String pomocniczy = ""; // bedzie przechowywal znaki jednego slowa podzielone przez | oraz ^
+	    String poczatkowy = ""; // bedzie przechowywal caly String pobierany z tablicySlow
+	    ArrayList<String> dzielenieSlow = new ArrayList<String>();
+	    
+	    char znak;
+	    
+	    int pom;
+	    
+	    for (int columna = 0; columna < tablicaSlow.size(); columna++)
+	    {
+	    	for(int szereg = 0;szereg < tablicaSlow.get(columna).size(); szereg++)
+	    	{
+	            poczatkowy = tablicaSlow.get(columna).get(szereg);
+	            
+	            for(int x = 0; x < poczatkowy.length(); x++)
+	            {
+	            	znak = poczatkowy.charAt(x);
+	            	if (znak == '|' || znak == '&' || znak == '(' || znak == ')')
+	            	{
+	            		if (!pomocniczy.isEmpty())
+	            		{
+	            			dzielenieSlow.add(pomocniczy);
+	            		}
+	            		dzielenieSlow.add(String.valueOf(znak));
+	            		
+	            		pomocniczy = "";
+	            	}
+	            	else if (znak == '=' && x+1 < poczatkowy.length() && poczatkowy.charAt(x+1) == '>')
+	            	{
+	            		if (!pomocniczy.isEmpty())
+	            		{
+	            			dzielenieSlow.add(pomocniczy);
+	            		}
+	            		dzielenieSlow.add("=>");
+	            	}
+	            	else if (znak == '<' && x+2 < poczatkowy.length() && poczatkowy.charAt(x+1) == '=' && poczatkowy.charAt(x+2) == '>')
+	            	{
+	            		if (!pomocniczy.isEmpty())
+	            		{
+	            			dzielenieSlow.add(pomocniczy);
+	            		}
+	            		dzielenieSlow.add("<=>");
+	            	}
+	            	//else if (znak == '>' && x-1 > 0 && poczatkowy.charAt(x-1) == '=')
+	            		
+	            	else pomocniczy += znak;
+	            	if (x+1 == poczatkowy.length() && znak != '|' && znak != '&' && znak != '(' && znak != ')') dzielenieSlow.add(pomocniczy);
+	            	//if (szereg+1 == tablicaSlow.size()) dzielenieSlow.add(pomocniczy);
+	            	//else if(x == poczatkowy.length())
+	            	//{
+	            	//	
+	            	//	dzielenieSlow.add(pomocniczy);
+	            	//}
+	            	//else pomocniczy += znak;
+	            }
+	            pomocniczy = "";
+	    	}
+    	
+	        // usuwanie zawartosci tablicy
+	        pom = tablicaSlow.get(columna).size();
+	        for (int x = 0; x < pom; x++)
+	        {
+	        	tablicaSlow.get(columna).remove(0);
+	        }
+	        
+	        // wstawianie nowych wartosci do tablicySlow i usuwanie ich z dzielenieSlow
+	        
+	        pom = dzielenieSlow.size();
+	        for (int x = 0; x < pom; x++)
+	        {
+	        	tablicaSlow.get(columna).add(dzielenieSlow.get(x));
+	        }
+	    	dzielenieSlow.clear();
         
-        
-        String pomocniczy = ""; // bedzie przechowywal znaki jednego slowa podzielone przez | oraz ^
-        String poczatkowy = ""; // bedzie przechowywal caly String pobierany z tablicySlow
-        ArrayList<String> dzielenieSlow = new ArrayList<String>();
-        
-        char znak;
-        
-        int pom;
-        
-        for (int columna = 0; columna < tablicaSlow.size(); columna++)
-        {
-        	for(int szereg = 0;szereg < tablicaSlow.get(columna).size(); szereg++)
-        	{
-                poczatkowy = tablicaSlow.get(columna).get(szereg);
-                
-                for(int x = 0; x < poczatkowy.length(); x++)
-                {
-                	znak = poczatkowy.charAt(x);
-                	if (znak == '|' || znak == '&')
-                	{
-                		if (!pomocniczy.isEmpty())
-                		{
-                			dzielenieSlow.add(pomocniczy);
-                		}
-                		dzielenieSlow.add(String.valueOf(znak));
-                		
-                		pomocniczy = "";
-                	}
-                	else pomocniczy += znak;
-                	if (x+1 == poczatkowy.length() && znak != '|' && znak != '&') dzielenieSlow.add(pomocniczy);
-                	//if (szereg+1 == tablicaSlow.size()) dzielenieSlow.add(pomocniczy);
-                	//else if(x == poczatkowy.length())
-                	//{
-                	//	
-                	//	dzielenieSlow.add(pomocniczy);
-                	//}
-                	//else pomocniczy += znak;
-                }
-                pomocniczy = "";
-        	}
-        	
-            // usuwanie zawartosci tablicy
-            pom = tablicaSlow.get(columna).size();
-            for (int x = 0; x < pom; x++)
-            {
-            	tablicaSlow.get(columna).remove(0);
-            }
-            
-            // wstawianie nowych wartosci do tablicySlow i usuwanie ich z dzielenieSlow
-            
-            pom = dzielenieSlow.size();
-            for (int x = 0; x < pom; x++)
-            {
-            	tablicaSlow.get(columna).add(dzielenieSlow.get(x));
-            }
-        	dzielenieSlow.clear();
-            
-        }
-        
-        // TODO tutaj zapierdalamy !!!!
+	    }
+	    
+	    // TODO tutaj zapierdalamy !!!!
+	
+	    
+	    System.out.println("___");
+	    
+	    
+	    System.out.println(tablicaSlow);
+	    
+	}
+	
+	public void wypelnijListe()
+	{
+		dzielSlowa();
 
-        
-        System.out.println("___");
-        
-        
-        System.out.println(tablicaSlow);
-        
-		
 	}
 	
 	//public 
