a6ec6546b165b0fa7f29563fb8bdc60bc56c59c0
andrzej <andrzejdackiewicz@gmail.com>
2015-05-12 22:43:27 +0200

    dfghjk

---
+++ src/model/OperacjeNaDrzewach.java
@@ -0,0 +1,18 @@
+package model;
+
+public class OperacjeNaDrzewach
+{
+	private ListaDrzew drzewoPredykatow;
+	private ListaDrzew drzewoTezy;
+	
+	public OperacjeNaDrzewach()
+	{
+		
+	}
+	
+	public void Oblicz()
+	{
+		
+	}
+	
+}
