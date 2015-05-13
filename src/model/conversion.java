5d08841a29f2c71d20eb3f7e67d8201dcd7e80a7
Kevin Katowitzer <Kevin Katowitzer@KevinKatowitzer>
2015-05-14 00:43:31 +0200

    trolololol

--- src/model/conversion.java
+++ src/model/conversion.java
@@ -1,50 +1,50 @@
-package model;
+package conversion;
 
 import javax.swing.tree.TreeNode;
 
 public class conversion {
-	public void Negation(TreeNode m){
-		if (m.getspojnik()==0){
-			m.setznak(1);
+	public void Negation(WezelDrzewa m){//Zdanie to Literal
+		if (m.getSpojnik()==0){
+			m.setZnak(true);
 		}
-		else if(m.getspojnik()==1){
-			m.setznak(2);
-			this.Negation(m.getChildAt(0));
-			this.Negation(m.getChildAt(1));
+		else if(m.getSpojnik()==1){
+			m.setSpojnik(2);
+			this.Negation(m.getLewy());
+			this.Negation(m.getPrawy());
 		}
-		else if(m.getspojnik()==2){
-			m.setznak(1);
-			this.Negation(m.getChildAt(0));
-			this.Negation(m.getChildAt(1));
+		else if(m.getSpojnik()==2){ // Klauzula to wektor literalow
+			m.setSpojnik(1);
+			this.Negation(m.getLewy()); 
+			this.Negation(m.getPrawy());
 		}
-		else if(m.getspojnik()==3){
-			m.setznak(2);
-			this.Negation(m.getChildAt(1));
+		else if(m.getSpojnik()==3){
+			m.setSpojnik(2);
+			this.Negation(m.getPrawy());
 		}
 		else {
-			ml = new TreeNode();
-			mp = new TreeNode();
-			ml.setChildAt(0) = m.getChildAt(0);
-			ml.setChildAt(1) = m.getChildAt(1);
-			mp.setChildAt(0) = m.getChildAt(0);
-			mp.setChildAt(1) = m.getChildAt(1);
-			ml.setznak(1);
-			mp.setznak(1);
-			m.setznak(2);
-			this.Negation(mp.getChildAt(0));
-			this.Negation(mp.getChildAt(1));
+			WezelDrzewa ml = new WezelDrzewa();
+			WezelDrzewa mp = new WezelDrzewa();
+			ml.setLewy(m.getLewy());
+			ml.setPrawy(m.getPrawy());
+			mp.setLewy(m.getLewy());
+			mp.setPrawy(m.getPrawy());
+			ml.setSpojnik(1);
+			mp.setSpojnik(1);
+			m.setZnak(true);;
+			this.Negation(mp.getLewy());
+			this.Negation(mp.getPrawy());
 		}
 	}
 	public void RedKON(TreeNode m){
-		if (m.getspojnik()==2) {
+		if (m.getSpojnik()==2) {
 			if (m.getParent()){
 				if (m.getParent().getspojnik==1){
-				mpr = new TreeNode();
+				WezelDrzewa mpr = new WezelDrzewa();
 				mpr.setParent(m.getParent());
-				mpr.setspojnik(1);
+				mpr.setSpojnik(1);
 				mpr.setChildAt(0,m.getChildAt(1));
 				mpr.setChildAt(1,m.getParent().getChildAt(1));
-				m.setspojnik(1);
+				m.setSpojnik(1);
 				}
 				m.getParent().setspojnik(2);
 				
@@ -59,18 +59,18 @@ public class conversion {
 		
 	}
 	
-	public void IMPEQ(TreeNode m){
+	public void IMPEQ(WezelDrzewa m){
 		if(m){
-		if (m.getspojnik()==3){
-			Negation(m.getChildAt(0));
-			m.setspojnik(1);
+		if (m.getSpojnik()==3){//kopia do ListaDrzew
+			Negation(m.getLewy());//funkcja g³owna wywolujace wszystko 
+			m.setSpojnik(1);
 			
 		}else if(m.getspojnik()==4){
-			mpl = new TreeNode();
-			mpr = new TreeNode();
-			mpl.setspojnik(1);
-			mpr.setspojnik(1);
-			mpl.setChildAt(0) = m.getChildAt(0);
+		 	WezelDrzewa mpl = new WezelDrzewa();
+			WezelDrzewa mpr = new WezelDrzewa();
+			mpl.setSpojnik(1);
+			mpr.setSpojnik(1);
+			mpl.setLewy(m.) = m.getChildAt(0);
 			Negation(mpl.GetChildAt(0));
 			mpl.setChildAt(1) = m.getChildAt(1);
 			mpr.setChildAt(0) = m.getChildAt(0);
