16a6842153080b772624512626a21ae675d9f60a
Mateusz <Mateusz@mat123456>
2015-05-15 19:42:47 +0200

    naprawa kilku spraw

--- src/model/OperacjeNaDrzewach.java
+++ src/model/OperacjeNaDrzewach.java
@@ -144,6 +144,7 @@ public class OperacjeNaDrzewach
                 {
                     wypisywanie(klauzule.get(x));
 		}
+                wytwarzajNoweKlauzuleNaPodstawieObecnych();
 	}
 
     public ListaDrzew getPredykaty()
