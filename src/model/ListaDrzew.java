b9c313b659202422cdf22a75a33f579edb0931fa
andrzej <andrzejdackiewicz@gmail.com>
2015-05-27 02:30:00 +0200

    asdfghgfda

--- src/model/ListaDrzew.java
+++ src/model/ListaDrzew.java
@@ -158,7 +158,6 @@ public class ListaDrzew {
 	            	znak = poczatkowy.charAt(x);
 	            	if (znak == '|' || znak == '&' || znak == '(' || znak == ')' || znak == '-')
 	            	{
-	            		System.out.println("a");
 	            		if (!pomocniczy.isEmpty())
 	            		{
 	            			dzielenieSlow.add(pomocniczy);
@@ -169,8 +168,7 @@ public class ListaDrzew {
 	            	}
 	            	else if ((znak == '=') && (x+1 < poczatkowy.length()) && (poczatkowy.charAt(x+1) == '>') && (x-1 >= 0) && (poczatkowy.charAt(x-1) != '<'))
 	            	{
-	            		System.out.println("b");
-
+	            		
 	            		if (!pomocniczy.isEmpty())
 	            		{
 	            			dzielenieSlow.add(pomocniczy);
@@ -181,7 +179,6 @@ public class ListaDrzew {
 	            	}
 	            	else if (znak == '<' && x+2 < poczatkowy.length() && poczatkowy.charAt(x+1) == '=' && poczatkowy.charAt(x+2) == '>')
 	            	{
-	            		System.out.println("c");
 
 	            		if (!pomocniczy.isEmpty())
 	            		{
